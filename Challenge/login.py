d68762e5536ec1ccf53497241af6d6806f62b544
Anian Sollinger <anian@sollingers.de>
2021-11-02 21:14:56 +0100

    Update login.py
    Made the settings command a defined function and moved it to the top

--- Challenge/login.py
+++ Challenge/login.py
@@ -1,5 +1,16 @@
 #We are creating a user login and registration
 
+def settingscommand(): #defines a function with the name settingscommand()
+
+    user_input = input() #Just a variable
+    
+    print("\nIf you want to see your account informations, type 'settings'")
+
+    if user_input == "settings": #checks if the user_input is equal to settings
+        print("\nHere is your account information:\n\nUsername: " + username + "\nPassword: " + password)
+
+#Start of the actual Program
+
 print("Welcome to room 847")
 
 #An answer is being requested
@@ -15,7 +26,7 @@ if(answer == "yes"):
 
     print("\n" * 3 + "Welcome to room 847 " + username + "!")
 
-
+    settingscommand()
     
 #If the answer is no, you will be sent to the registration
 elif(answer == "no"): 
@@ -29,13 +40,8 @@ elif(answer == "no"):
 
     print("\n" *5 +  "Welcome " + username + "! Your account with the e-mail " + email + ", has been succesfully created!")
 
+    settingscommand()
+
 #if the answer is not "yes" or "no" then it gives you a syntax error
 else:
     print("Syntax error: try yes or no")
-
-print("\nIf you want to see your account informations, type 'settings'")
-
-command_input = input()
-
-if command_input == "settings":
-    print("\nHere is your account information:\n\nUsername: " + username + "\nPassword: " + password)
