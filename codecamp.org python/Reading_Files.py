0de2599e5389f4ac2ae629c13521660280f72a04
Anian Sollinger <anian@sollingers.de>
2021-11-02 22:08:10 +0100

    Update Reading_Files.py

--- codecamp.org python/Reading_Files.py
+++ codecamp.org python/Reading_Files.py
@@ -1,5 +1,5 @@
 #Using open()   handle = open(filename, mode)
 
-fhand = open('mbox.txt', "r") #opening a file called "mbox.txt" with the read mode
+fhand = open(r"C:\Users\anian\OneDrive\Dokumente\GitHub\Documents\codecamp.org python\mbox.txt", "r", encoding="UTF-8") #opening a file called "mbox.txt" with the read mode. For the file, use "r" before the String to make it a raw string
 
 print(fhand)
